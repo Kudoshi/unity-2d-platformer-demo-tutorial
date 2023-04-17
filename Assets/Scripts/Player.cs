72fe62a82bc7263dd132cddf69b215b8d1f8ddbe
Kudoshi <brendenguan@live.com>
2023-04-17 12:51:39 +0800

    Display winning UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public TextMeshProUGUI CoinLabel;
+    public TextMeshProUGUI UI_CoinCountLabel;
+    public GameObject Winning_UI;
 
     private int m_Coins;
+    private Vector3 m_RespawnPoint;
+    private Rigidbody2D m_Rb;
+    private PlayerMovement m_playerMovement;
+
+    private void Start()
+    {
+        m_RespawnPoint = transform.position;
+        m_Rb = GetComponent<Rigidbody2D>();
+        m_playerMovement = GetComponent<PlayerMovement>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,6 +30,23 @@ public class Player : MonoBehaviour
             CoinLabel.text = m_Coins.ToString();
             Debug.Log(m_Coins);
             Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("DeathCollider"))
+        {
+            transform.position = m_RespawnPoint;
+            m_Rb.velocity = Vector3.zero;
+            Debug.Log("Respawned!");
+        }
+
+        if (collision.CompareTag("WinningFlag"))
+        {
+            UI_CoinCountLabel.text = m_Coins.ToString();
+            Winning_UI.SetActive(true);
+            m_Rb.velocity = Vector3.zero;
+            m_playerMovement.enabled = false;
+
+            Debug.Log("WIN!");
         }
     }
 }
