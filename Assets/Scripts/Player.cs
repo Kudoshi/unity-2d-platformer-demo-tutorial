7ef39b8633533b0faa546b6dd7067d1eb163ce64
Kudoshi <brendenguan@live.com>
2023-04-17 12:56:35 +0800

    Create to next scene script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -46,7 +47,14 @@ public class Player : MonoBehaviour
             m_Rb.velocity = Vector3.zero;
             m_playerMovement.enabled = false;
 
+            GoToNextScene();
+
             Debug.Log("WIN!");
         }
+    }
+
+    private void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
